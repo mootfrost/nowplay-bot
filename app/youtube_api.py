08cf640efce55db0b760b4e909d453fd43b1c494
Mootfrost <andrey13102007@gmail.com>
2025-04-14 21:58:07 +0300

    Attempt to switch to video search instead of songs

--- app/youtube_api.py
+++ app/youtube_api.py
@@ -10,8 +10,8 @@ from app.config import config
 
 ytmusic = YTMusic('oauth.json',
                   oauth_credentials=OAuthCredentials(client_id=config.yt.client_id, client_secret=config.yt.client_secret))
-if config.proxy:
-    ytmusic.proxies = {'http': config.proxy, 'https': config.proxy}
+# if config.proxy:
+#     ytmusic.proxies = {'http': config.proxy, 'https': config.proxy}
 
 
 def name_to_youtube(name: str):
